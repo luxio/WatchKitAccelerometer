501570ba66ea7808ce614ee5b45f793b01a20eda
Stephane Lux <stephane@lux.io>
2016-04-21 14:22:46 +0200

    Initial Commit

---
+++ WatchKitAccelerometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKitAccelerometer
+//
+//  Created by Stéphane Lux on 21.04.16.
+//  Copyright © 2016 LUXio IT-Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
